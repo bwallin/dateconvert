778ed990c9079dea34b0538961ef70ede519e7c9
kpurdon <kylepurdon@gmail.com>
2014-09-30 19:36:28 -0600

    Correct code and test.

--- day_of_year.py
+++ day_of_year.py
@@ -2,11 +2,19 @@ import click
 import datetime as dt
 
 
+def convert_to_datetime(yyyymmdd):
+    return dt.datetime.strptime(yyyymmdd, '%Y%m%d')
+
+
+def calculate_day_of_year(yyyymmdd_dt):
+    return yyyymmdd_dt.strftime('%j')
+
+
 @click.command()
 @click.argument('yyyymmdd')
 def main(yyyymmdd):
-    input_date = dt.datetime.strptime(yyyymmdd, '%Y%m%d')
-    day_of_year = input_date.strftime('%j')
+    yyyymmdd_dt = convert_to_datetime(yyyymmdd)
+    day_of_year = calculate_day_of_year(yyyymmdd_dt)
     print(day_of_year)
 
 
---
+++ tests/test_day_of_year.py
@@ -0,0 +1,33 @@
+import unittest
+import day_of_year
+import datetime as dt
+
+
+class TestDayOfYear(unittest.TestCase):
+
+    def setUp(self):
+        self.yyyymmdd = '20110101'
+        self.yyyymmdd_dt = dt.datetime(2011, 01, 01)
+
+    def test_result_is_datetime(self):
+
+        actual = day_of_year.convert_to_datetime(self.yyyymmdd)
+        expected = True
+
+        self.assertEqual(expected, isinstance(actual, dt.datetime))
+
+
+    def test_datetime_is_correct(self):
+
+        actual = day_of_year.convert_to_datetime(self.yyyymmdd)
+        expected = self.yyyymmdd_dt
+
+        self.assertEqual(expected, actual)
+
+
+    def test_result_is_correct(self):
+
+        actual = day_of_year.calculate_day_of_year(self.yyyymmdd_dt)
+        expected = '001'
+
+        self.assertEqual(expected, actual)
