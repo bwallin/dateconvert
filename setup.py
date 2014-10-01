8b07a5c0ae6cf98828fd09b8b628c9c66c649a26
kpurdon <kylepurdon@gmail.com>
2014-09-30 20:31:27 -0600

    Create a python package.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setuptools_kwargs = {
+    'install_requires': [
+        'click>=3.3,<4'
+    ],
+    'entry_points': {
+        'console_scripts': [
+            'day_of_year = day_of_year.day_of_year:main'
+        ]
+    }
+}
+
+setup(
+    name='pydate',
+    description='Calculates the day of year from a given YYYYMMDD.',
+    author='Kyle W Purdon',
+    author_email='kylepurdon@gmail.com',
+    url='https://github.com/kpurdon/pydate',
+    version='0.0.1',
+    packages=find_packages(),
+    **setuptools_kwargs
+)
