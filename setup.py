6bb25464c2f414ffc4eef9f9a094b46e75735c82
kpurdon <kylepurdon@gmail.com>
2014-10-05 13:54:32 -0600

    Add metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from setuptools import setup, find_packages
 
 
+classifiers = [
+    'Environment :: Console',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7'
+]
+
 setuptools_kwargs = {
     'install_requires': [
         'click>=3.3,<4'
@@ -29,5 +39,6 @@ setup(
     url='https://github.com/kpurdon/dateconvert',
     version=get_version(),
     packages=find_packages(),
+    classifiers=classifiers,
     **setuptools_kwargs
 )
